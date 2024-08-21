7cd16cad2fcc50f543a425bda21d0e91ca6a41aa
professorjiwon <professorjiwon@gmail.com>
2024-08-21 17:54:35 +0900

    08.21_7교시_bootstrap

--- 10.bootstrap/src/component/Header.js
+++ 10.bootstrap/src/component/Header.js
@@ -1,8 +1,13 @@
 import logo from './logo.png';
 import './common.css';
 import { Button, Modal, Form } from 'react-bootstrap';
+import { useState } from 'react';
 
 function Header() {
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
     return (
         <>
             <header>
@@ -10,43 +15,43 @@ function Header() {
                 <h2>The Joeun</h2>
                 <div className="login">
                     <Button variant="outline-secondary" size="sm">회원가입</Button>&emsp;
-                    <Button variant="outline-success" size="sm">로 그 인</Button>
+                    <Button variant="outline-success" size="sm" onClick={handleShow}>로 그 인</Button>
                 </div>
+                <Login show={show} onHide={handleClose} />
             </header>
         </>
     )
-}
 
-function Login() {
-    return (
-        <>
-            <Modal show={show} onHide={handleClose}>
-            <Modal.Header closeButton>
-                <Modal.Title>Login</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-                <Form>
-                    <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                        <Form.Label>ID</Form.Label>
-                        <Form.Control type="email" placeholder="name@example.com" />
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-                        <Form.Label>PW</Form.Label>
-                        <Form.Control type="password" />
-                    </Form.Group>
-                </Form>
-            </Modal.Body>
-            <Modal.Footer>
-                <Button variant="secondary" onClick={handleClose}>
-                    Close
-                </Button>
-                <Button variant="primary" onClick={handleClose}>
-                    Login
-                </Button>
-            </Modal.Footer>
-        </Modal>
-        </>
-    )
+    function Login() {
+        return (
+            <>
+                <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Login</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form>
+                        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                            <Form.Label>ID</Form.Label>
+                            <Form.Control type="email" placeholder="name@example.com" />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                            <Form.Label>PW</Form.Label>
+                            <Form.Control type="password" />
+                        </Form.Group>
+                    </Form>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Close
+                    </Button>
+                    <Button variant="primary" onClick={handleClose}>
+                        Login
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+            </>
+        )
+    }
 }
-
 export default Header;
