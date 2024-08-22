9f5df996d3a664f7bc26693a89456b1169e1d2d0
professorjiwon <professorjiwon@gmail.com>
2024-08-22 14:42:14 +0900

    08.22_4교시

--- 11.instar/src/App.js
+++ 11.instar/src/App.js
@@ -5,25 +5,17 @@ function App() {
   let [count, setCount] = useState([0,0,0]);
   let [title, setTitle] = useState(["초밥", "찬란한 아구", "역전우동"]);
   let [modal, setModal] = useState(false);
-  let [modalTitle, setModalTitle] = useState(0);  // div의 index번호 변경. [0,1,2]
+  let [modalIndex, setModalIndex] = useState(0);  // div의 index번호 변경. [0,1,2]
+  let [inputValue, setInputValue] = useState("");
 
   return (
     <div className="App">
       <h2 className="title">맛집 추천 INSTAR</h2>
-
-      {/* 원본이 바뀜 */}
-      {/* <button onClick={() => {setTitle(["청담등심", "찬란한 아구", "역전우동"]);}}>글수정</button> */}
-      <button onClick={() => {
-        let copy = [...title];
-        copy[0] = "청담등심";
-        setTitle(copy);
-      }}>글수정</button>
-
       {
         title.map(function(t, i) {
           return (
             <div className="list" key={i}>
-              <h4 onClick={() => {setModal(!modal); setModalTitle(i)}}>{t}</h4>
+              <h4 onClick={() => {setModal(!modal); setModalIndex(i)}}>{t}</h4>
               <p>8월 22일  <span onClick={() => {
                   let copy = [...count];
                   copy[i] = copy[i] + 1;
@@ -34,11 +26,14 @@ function App() {
         })
       }
 
-      
+      <input onChange={(e) => {setInputValue(e.target.value)}} />
+      <button onClick={() => {
+        let copy = [...title];
+        copy.unshift(inputValue);
+        setTitle(copy)
+      }}>글추가</button>
 
-
-      { modal ? <Modal title={title} modalTitle={modalTitle} /> : null }
-
+      { modal ? <Modal title={title} modalIndex={modalIndex} setTitle={setTitle} /> : null }
     </div>
   );
 }
@@ -46,9 +41,17 @@ function App() {
 function Modal(props) {
   return (
     <div className="modal">
-      <h4>{props.title[props.modalTitle]}</h4>
+      <h4>{props.title[props.modalIndex]}</h4>
       <p>날짜</p>
       <p>상세내용</p>
+
+      {/* 원본이 바뀜 */}
+      {/* <button onClick={() => {setTitle(["청담등심", "찬란한 아구", "역전우동"]);}}>글수정</button> */}
+      <button onClick={() => {
+        let copy = [...props.title];
+        copy[props.modalIndex] = "청담등심";
+        props.setTitle(copy);
+      }}>글제목수정</button>
     </div>
   )
 }
